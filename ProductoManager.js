1793429ee9d0aca4951552538626b41e4cd10987
Emiliano Ramos <119552246+Memo1302@users.noreply.github.com>
2023-07-31 21:08:15 -0300

    preentrega

---
+++ ProductoManager.js
@@ -0,0 +1,20 @@
+class ProductManager {
+
+       constructor() { 
+    }
+    getProducts() {
+    }
+    addProducts(titel, description, price, thumnail, code, stock){
+    }
+    getProductById(id) {
+    }
+};
+
+const pm = new ProductoManager ();
+
+pm.addProducts();
+
+console.log("from", pm.getProductById());
+const product = pm.getProductById(5);
+
+console.log("search", product);
